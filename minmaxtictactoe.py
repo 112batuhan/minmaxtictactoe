175a3d72be14f258263e728debba09dad98bbac2
112batuhan <36697363+112batuhan@users.noreply.github.com>
2020-12-01 12:19:35 +0300

    initial commit

---
+++ minmaxtictactoe.py
@@ -0,0 +1,161 @@
+import copy
+
+def check_win(board_x, board_o):
+    for x_row, o_row in zip(board_x, board_o):
+        if all(x_row):
+            return 1
+        elif all(o_row):
+            return -1
+
+    transposed_x = [list(i) for i in zip(*board_x)]
+    transposed_y = [list(i) for i in zip(*board_o)]
+    for x_row, o_row in zip(transposed_x, transposed_y):
+        if all(x_row):
+            return 1
+        elif all(o_row):
+            return -1
+    
+
+    if all([row[i] for i,row in enumerate(board_x)]) or all([row[-i-1] for i,row in enumerate(board_x)]):
+        return 1
+    elif all([row[i] for i,row in enumerate(board_o)]) or all([row[-i-1] for i,row in enumerate(board_o)]):
+        return -1
+
+    
+
+
+count = 0
+
+def max_value(board_x,board_o):
+    global count
+    count +=1
+    result = check_win(board_x, board_o)
+    if result != 0: 
+        return result
+        
+    v = -10000
+    i=0
+    while(i<3):
+        k=0
+        while(k<3):
+            if board_x[i][k] != True and board_o[i][k] != True:
+                new_board_x = copy.deepcopy(board_x)
+                new_board_o = copy.deepcopy(board_o)
+                new_board_x[i][k] = True
+                v = max(v, min_value(new_board_x, new_board_o))
+            k += 1
+        i += 1
+                
+    return v
+
+def min_value(board_x, board_o):
+    global count
+    count +=1
+    result = check_win(board_x, board_o)
+    if result != 0:
+        return result
+    
+    v = 10000
+    m=0
+    while(m<3):
+        n=0
+        while(n<3):
+            if board_x[m][n] != True and board_o[m][n] != True:
+                new_board_x = copy.deepcopy(board_x)
+                new_board_o = copy.deepcopy(board_o)
+                new_board_o[m][n] = True
+                v = min(v, max_value(new_board_x, new_board_o))
+            n += 1
+        m += 1
+
+    return v
+
+
+
+count_ab = 0
+
+def max_value_ab(board_x,board_o, alpha, beta):
+    global count_ab
+    count_ab +=1
+    result = check_win(board_x, board_o)
+    if result != 0: 
+        return result
+        
+    v = -10000
+    i=0
+    while(i<3):
+        k=0
+        while(k<3):
+            if board_x[i][k] != True and board_o[i][k] != True:
+                new_board_x = copy.deepcopy(board_x)
+                new_board_o = copy.deepcopy(board_o)
+                new_board_x[i][k] = True
+                v = max(v, min_value_ab(new_board_x, new_board_o, alpha, beta))
+                if (v >= beta): 
+                    return v
+                alpha = max(alpha, v)
+
+            k += 1
+        i += 1
+                
+    return v
+
+def min_value_ab(board_x, board_o, alpha, beta):
+    global count_ab
+    count_ab +=1
+    result = check_win(board_x, board_o)
+    if result != 0:
+        return result
+    
+    v = 10000
+    i=0
+    while(i<3):
+        k=0
+        while(k<3):
+            if board_x[i][k] != True and board_o[i][k] != True:
+                new_board_x = copy.deepcopy(board_x)
+                new_board_o = copy.deepcopy(board_o)
+                new_board_o[i][k] = True
+                v = min(v, max_value_ab(new_board_x, new_board_o, alpha, beta))
+                if (v <= alpha): 
+                    return v
+                beta = min(beta, v)
+
+            k += 1
+        i += 1
+
+    return v
+
+
+def minmax(board_x, board_o, ab = False):
+    
+    (val, action) = (-10000, None)
+    i=0
+    while(i<3):
+        k=0
+        while(k<3):
+            if board_x[i][k] != True and board_o[i][k] != True:
+                new_board_x = copy.deepcopy(board_x)
+                new_board_o = copy.deepcopy(board_o)
+                new_board_x[i][k] = True
+
+                if ab:
+                    v = min_value_ab(new_board_x, new_board_o, -10000, 10000)
+                else:
+                    v = min_value(new_board_x, new_board_o)
+
+                if  val <= v:
+                   (val, action) = (v, (i,k))
+            k += 1
+        i += 1
+    return action
+
+
+#board_x = [[False,False,False],[False,False,False],[False,False,False]]
+#board_o = [[False,False,False],[False,False,False],[False,False,False]]
+
+#max_value(board_x, board_o)
+#max_value_ab(board_x, board_o, -10000, 10000)
+#print(count, count_ab, count/count_ab)
+
+#print(minmax(board_x,board_o, True))
---
+++ tictactoe.py
@@ -0,0 +1,119 @@
+from minmaxtictactoe import minmax
+
+class tictactoe:
+
+    def __init__(self):
+ 
+        self.reset_board()
+
+    def reset_board(self):
+
+        self.x_map = [[False,False,False],[False,False,False],[False,False,False]]
+        self.o_map = [[False,False,False],[False,False,False],[False,False,False]]
+
+
+    def make_move(self, player, row, col):
+
+        if (row>3 or row<1) or (row>3 or row<1):
+            print("Dimensions out of range, pls enter between 1 and 3.")
+            return False
+        
+        row = row-1
+        col = col-1
+
+        if self.x_map[row][col] == True or self.o_map[row][col] == True:
+            print("There is already a piece there.")
+            return False
+
+        if player == 0:
+            self.x_map[row][col] = True
+        else:
+            self.o_map[row][col] = True
+        
+        return True
+
+
+        
+    def show_board(self):
+        #todo: make it friendly to this class
+
+        whole = []
+        for i, (x_row, o_row) in enumerate(zip(self.x_map, self.o_map)):
+            line = ""    
+            for k, (x_col, o_col) in enumerate(zip(x_row, o_row)):
+                
+                if x_col == True:
+                    line += "X"
+                elif o_col == True:
+                    line += "O"
+                else:
+                    line += " "
+
+                if k<2:
+                    line += "|"
+            
+            whole.append(line)
+            if i<2:
+                whole.append("-----")
+
+        print("\n".join(whole))
+
+
+    def check_win(self):
+        for x_row, o_row in zip(self.x_map, self.o_map):
+            if all(x_row):
+                return 1
+            elif all(o_row):
+                return -1
+
+        transposed_x = [list(i) for i in zip(*self.x_map)]
+        transposed_y = [list(i) for i in zip(*self.o_map)]
+        for x_row, o_row in zip(transposed_x, transposed_y):
+            if all(x_row):
+                return 1
+            elif all(o_row):
+                return -1
+        
+
+        if all([row[i] for i,row in enumerate(self.x_map)]) or all([row[-i-1] for i,row in enumerate(self.x_map)]):
+            return 1
+        elif all([row[i] for i,row in enumerate(self.o_map)]) or all([row[-i-1] for i,row in enumerate(self.o_map)]):
+            return -1
+
+        return 0 
+
+    def get_board(self):
+        return (self.x_map, self.o_map)
+
+
+game = tictactoe()
+
+finished = False
+while not finished:
+    player_move = [int(x) for x in input("your turn:").split(",")]
+    print(player_move)
+    game.make_move(1, *player_move)
+    game.show_board()
+    status = game.check_win()
+    if status == 1:
+        finished = True
+        print("X Won!")
+        break
+    elif status == -1:
+        finished = True
+        print("O Won!")
+        break
+
+    ai_move = [x+1 for x in minmax(*game.get_board(),True)]
+    print(ai_move)
+    game.make_move(0, *ai_move)
+    game.show_board()
+    status = game.check_win()
+    if status == 1:
+        finished = True
+        print("X Won!")
+        break
+    elif status == -1:
+        finished = True
+        print("O Won!")
+        break
